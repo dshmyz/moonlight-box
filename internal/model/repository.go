b6324bbddeeade4b7030be62f23d3db6e26b9de8
gracegaoya <183714713@qq.com>
2026-05-01 09:03:57 +0800

    feat(model): add PackageTypes field to Repository for multi-type virtual repos

--- internal/model/repository.go
+++ internal/model/repository.go
@@ -38,6 +38,9 @@ type Repository struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 
+	// PackageTypes JSON 数组字符串，用于虚拟仓库支持多种包类型
+	PackageTypes string `json:"package_types" gorm:"type:text"`
+
 	Members []RepositoryGroup `json:"members,omitempty" gorm:"foreignKey:VirtualRepoID"`
 }
 
