cb4fc4312cc3eea70e9c459c011c8d1b77ef73a7
gracegaoya <183714713@qq.com>
2026-05-02 19:31:30 +0800

    feat: 在仓库表单中添加元数据同步配置

--- web/src/components/repository/RepositoryFormDialog.vue
+++ web/src/components/repository/RepositoryFormDialog.vue
@@ -52,6 +52,16 @@
           <TimeoutConfigForm :form="formData" />
         </el-tab-pane>
 
+        <el-tab-pane v-if="formData.type === 'proxy'" name="sync">
+          <template #label>
+            <span class="tab-label">
+              <el-icon><Refresh /></el-icon>
+              元数据同步
+            </span>
+          </template>
+          <MetadataSyncConfigForm :form="formData" />
+        </el-tab-pane>
+
         <el-tab-pane name="cache">
           <template #label>
             <span class="tab-label">
@@ -108,7 +118,7 @@
 import { ref, computed, watch } from 'vue'
 import type { FormInstance, FormRules } from 'element-plus'
 import { ElMessage } from 'element-plus'
-import { Document, Link, Setting, Coin, Lock, Connection } from '@element-plus/icons-vue'
+import { Document, Link, Setting, Coin, Lock, Connection, Refresh } from '@element-plus/icons-vue'
 import { repositoryApi, type Repository, type FailureCacheRule } from '@/api/repository'
 import BasicInfoForm from './BasicInfoForm.vue'
 import AuthConfigForm from './AuthConfigForm.vue'
@@ -116,6 +126,7 @@ import TimeoutConfigForm from './TimeoutConfigForm.vue'
 import CacheConfigForm from './CacheConfigForm.vue'
 import PermissionsConfigForm from './PermissionsConfigForm.vue'
 import VirtualMembersForm from './VirtualMembersForm.vue'
+import MetadataSyncConfigForm from './MetadataSyncConfigForm.vue'
 
 interface Props {
   modelValue: boolean
@@ -161,6 +172,9 @@ interface RepoFormData {
   failure_cache_rules: string
   allow_overwrite: boolean
   allow_delete: boolean
+  metadata_sync_enabled: boolean
+  metadata_sync_interval: number
+  sync_mode: 'metadata_only' | 'full'
 }
 
 const defaultFormData = (): RepoFormData => ({
@@ -183,6 +197,9 @@ const defaultFormData = (): RepoFormData => ({
   failure_cache_rules: '',
   allow_overwrite: false,
   allow_delete: false,
+  metadata_sync_enabled: false,
+  metadata_sync_interval: 3600,
+  sync_mode: 'metadata_only',
 })
 
 const formData = ref<RepoFormData>(defaultFormData())
@@ -252,6 +269,9 @@ watch(
           : '',
         allow_overwrite: repo.allow_overwrite ?? false,
         allow_delete: repo.allow_delete ?? false,
+        metadata_sync_enabled: repo.metadata_sync_enabled ?? false,
+        metadata_sync_interval: repo.metadata_sync_interval ?? 3600,
+        sync_mode: repo.sync_mode ?? 'metadata_only',
       }
 
       if (repo.auth_config) {
@@ -352,6 +372,9 @@ const buildSubmitData = (): Partial<Repository> => {
     data.max_redirects = formData.value.max_redirects
     data.insecure_skip_verify = formData.value.insecure_skip_verify
     data.failure_cache_rules = failureCacheRules
+    data.metadata_sync_enabled = formData.value.metadata_sync_enabled
+    data.metadata_sync_interval = formData.value.metadata_sync_interval
+    data.sync_mode = formData.value.sync_mode
   }
 
   if (formData.value.type === 'local' || formData.value.type === 'proxy') {
