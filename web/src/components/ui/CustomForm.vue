e3e56f38d88d7ffe3549eff42b3e2b5ad8bcf94d
gracegaoya <183714713@qq.com>
2026-05-05 15:23:45 +0800

    feat: add CustomForm component

---
+++ web/src/components/ui/CustomForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-form
+    :model="model"
+    :rules="rules"
+    :label-width="labelWidth"
+    :label-position="labelPosition"
+    class="custom-form"
+  >
+    <slot />
+  </el-form>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  model: object
+  rules?: object
+  labelWidth?: string
+  labelPosition?: 'left' | 'right' | 'top'
+}
+
+withDefaults(defineProps<Props>(), {
+  rules: {},
+  labelWidth: '100px',
+  labelPosition: 'right'
+})
+</script>
+
+<style scoped>
+.custom-form {
+  width: 100%;
+}
+</style>
