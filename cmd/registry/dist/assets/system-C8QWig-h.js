673a8eebc8732c48fd7dc9fe55a4e597c2fd7314
gracegaoya <183714713@qq.com>
2026-05-11 11:50:14 +0800

    build: update frontend dist assets

--- cmd/registry/dist/assets/system-C8QWig-h.js
+++
@@ -1,1 +0,0 @@
-import{L as t}from"./index-DajvYZpn.js";const o={getConfigs(){return t.get("/configs")},updateConfigs(s){return t.post("/configs",s)},getSystemInfo(){return t.get("/system/info")}};export{o as s};
